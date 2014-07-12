e9d2c65e171b2abafc97e6033b6aa837eb79f511
boscence <boscence@gmail.com>
2014-07-13 00:31:47 +1000

    My files for Exploritory Data Analysis Assessment 1

--- plot3.R
+++ plot3.R
@@ -12,6 +12,7 @@ subPower = subset(power, power$Date =="2007-02-01" | power$Date =="2007-02-02")
 subPower$dateTime = strptime( paste(subPower$Date,subPower$Time), format="%Y-%m-%d %H:%M:%S")
 
 ### make plot 3
+par(mfrow=c(1,1))
 with(subPower, plot(dateTime,Sub_metering_1, type="l", col = "Black"
                     , ylab = "Energy sub metering",xlab = ""
                     ,cex.lab=0.8
@@ -28,8 +29,11 @@ legend("topright"
 ##print plot3
 setwd("/home/ab/Dropbox/projects/JHU_DataScience/eda/project1/plots")
 dev.copy(png,'plot3.png', width = 480, height = 480, units = "px")
+par(mfrow=c(1,1))
 with(subPower, plot(dateTime,Sub_metering_1, type="l", col = "Black"
-                    , ylab = "Energy sub metering",xlab = ""))
+                    , ylab = "Energy sub metering",xlab = ""
+                    ,cex.lab=0.8
+                    ,cex.axis=0.8))
 with(subPower, lines(dateTime,Sub_metering_2, type="l", col = "Red"))
 with(subPower, lines(dateTime,Sub_metering_3, type="l", col = "Blue"))
 
@@ -37,4 +41,5 @@ legend("topright"
        , col = c("black", "red", "blue")
        , lty= 1
        , legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.off()
